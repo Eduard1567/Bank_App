55b876cce2ad50bba3b3178e9c1ec7e974276551
Eduard1567 <eduardenache1567@yahoo.com>
2023-01-11 17:25:02 +0200

    Finished project

---
+++ Bank_App - Copy/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_App.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public BankAccount Account { get; set; }
+
+
+
+        public User(string name, string username, string password, BankAccount account)
+        {
+            Name = name;
+            Username = username;
+            Password = password;
+            Account = account;
+        }
+    }
+}
